d645ca63ca53a82a5e4d4ed156540662b680f045
Douglas Liberalesso <dliberalesso@gmail.com>
2022-09-16 18:26:33 +0000

    Go back to Dracula PRO

--- home/colors.nix
+++ home/colors.nix
@@ -5,12 +5,9 @@
     colors = mkOption { type = attrs; };
   };
 
-  # Dracula PRO - Van Helsing
+  # Dracula PRO
   config.colors = {
     foreground = "#f8f8f2";
-    background = "#0b0d0f";
-    selection = "#414d58";
-    comment = "#708ca9";
     red = "#ff9580";
     orange = "#ffca80";
     yellow = "#ffff80";
@@ -18,5 +15,15 @@
     purple = "#9580ff";
     cyan = "#80ffea";
     pink = "#ff80bf";
+
+    # Base
+    background = "#22212C";
+    comment = "#7970A9";
+    selection = "#454158";
+
+    # Van Helsing
+    # background = "#0b0d0f";
+    # comment = "#708ca9";
+    # selection = "#414d58";
   };
 }
