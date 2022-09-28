f95163ece4a6c98795416dc84553c25da964009e
Douglas Liberalesso <dliberalesso@gmail.com>
2022-09-28 13:53:38 +0000

    Remove neovim-nightly for now

--- flake.nix
+++ flake.nix
@@ -2,29 +2,23 @@
   description = "My NixOS & Home-Manager config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = github:numtide/flake-utils;
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.utils.follows = "flake-utils";
     };
 
-    neovim-nightly = {
-      url = github:neovim/neovim?dir=contrib;
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-
     vscode-server = {
-      url = "github:msteen/nixos-vscode-server";
+      url = github:msteen/nixos-vscode-server;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
+      url = github:nix-community/NixOS-WSL;
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
