3320940371c8a09eadf2fe2fe79387a3e1e9aa62
Douglas Liberalesso <dliberalesso@gmail.com>
2024-05-26 19:37:45 +0000

    revert: "feat(nvim): add lualine and remove mini.statusline"

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,14 @@
 return {
   "echasnovski/mini.basics",
   {
+    "echasnovski/mini.statusline",
+    event = "VeryLazy",
+    dependencies = {
+      "rachartier/tiny-devicons-auto-colors.nvim",
+    },
+    opts = { set_vim_settings = false },
+  },
+  {
     "echasnovski/mini.ai",
     event = { "BufReadPost", "BufNewFile" },
     opts = { n_lines = 500 },
