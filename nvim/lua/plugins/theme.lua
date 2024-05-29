90d94ae9f5b488be3545aeacd8963faf71d9eaa8
Douglas Liberalesso <dliberalesso@gmail.com>
2024-05-29 22:16:06 +0000

    refactor(nvim): improve readability of theme.lua

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,52 +1,62 @@
 return {
   {
-    "rachartier/tiny-devicons-auto-colors.nvim",
-
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
+
+    dependencies = {
+      {
+        "rachartier/tiny-devicons-auto-colors.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+      },
+    },
 
     lazy = false,
 
     config = function(_, opts)
       require("catppuccin").setup(opts)
 
-      local palette = require("catppuccin.palettes").get_palette(opts.flavour)
-      require("tiny-devicons-auto-colors").setup { colors = palette }
+      require("tiny-devicons-auto-colors").setup {
+        colors = require("catppuccin.palettes").get_palette(opts.flavour),
+      }
 
       vim.cmd.colorscheme "catppuccin"
     end,
 
     opts = {
       flavour = "mocha",
+
       integrations = {
         gitsigns = true,
         indent_blankline = { enabled = true },
         markdown = true,
+
         mini = {
           enabled = true,
           indentscope_color = "mauve",
         },
+
         native_lsp = {
           enabled = true,
+
           virtual_text = {
             errors = { "italic" },
             hints = { "italic" },
             warnings = { "italic" },
             information = { "italic" },
           },
+
           underlines = {
             errors = { "underline" },
             hints = { "underline" },
             warnings = { "underline" },
             information = { "underline" },
           },
+
           inlay_hints = {
             background = true,
           },
         },
+
         neotree = true,
         noice = true,
         notify = true,
