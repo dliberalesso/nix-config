dacd06bf7aaa5719059909ade35d22899757b4f3
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-06 20:51:56 +0000

    feat(nvim): enable some plugins from astrocommunity

--- config/nvim/lua/community.lua
+++ config/nvim/lua/community.lua
@@ -6,6 +6,12 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.comment.ts-comments-nvim" },
+  { import = "astrocommunity.completion.cmp-emoji" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
   -- import/override with your plugins folder
 }
