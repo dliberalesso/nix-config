a0b41896a18c4ae872293c2a3932d6141f693abc
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-10 18:52:15 +0000

    feat(nvim): added a command to install treesitter parsers when using `nvim --headless`

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,31 @@
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "fish", "json", "jsonc", "just", "nix", "toml", "yaml" },
-    auto_install = true,
-    highlight = {
-      additional_vim_regex_highlighting = false,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "fish", "json", "jsonc", "just", "nix", "toml", "yaml" },
+      auto_install = true,
+      highlight = {
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
+  {
+    "AstroNvim/astrocore",
+
+    opts = {
+      commands = {
+        HeadlessTSUpdate = {
+          function()
+            local astrocore = require "astrocore"
+            local opts = astrocore.plugin_opts "nvim-treesitter"
+            local ensure_installed = opts.ensure_installed
+
+            vim.cmd.TSUpdateSync(ensure_installed)
+          end,
+          desc = "Install treesitter parsers declared in `ensure_installed`",
+        },
+      },
     },
   },
 }
