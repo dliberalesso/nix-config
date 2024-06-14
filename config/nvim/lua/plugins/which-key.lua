b5906be5a39b39ca039a2018ebe644fda1b7d8b1
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-14 19:54:22 +0000

    feat(nvim): registered keymap groups inside `which-key`

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -4,6 +4,23 @@ return {
 
   event = "VeryLazy",
 
+  init = function()
+    vim.opt.timeout = true
+    vim.opt.timeoutlen = 300
+    vim.opt.updatetime = 250
+  end,
+
+  config = function(_, opts)
+    local wk = require "which-key"
+    wk.setup(opts)
+
+    wk.register({
+      f = { name = "File" },
+      b = { name = "Buffer" },
+      g = { name = "Git" },
+    }, { prefix = "<leader>" })
+  end,
+
   opts = {
     icons = {
       group = "",
