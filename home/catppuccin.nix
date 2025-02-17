bcbb9d451c1b7304fe8ee44ef9b85958de37f50f
Douglas Liberalesso <dliberalesso@gmail.com>
2025-02-17 20:23:21 +0000

    fix(catppuccin): fix options that were renamed in upstream

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -1,14 +1,6 @@
 {
-  catppuccin.flavor = "mocha";
-
-  programs = {
-    bat.catppuccin.enable = true;
-    bottom.catppuccin.enable = true;
-    fish.catppuccin.enable = true;
-    fzf.catppuccin.enable = true;
-    git.delta.catppuccin.enable = true;
-    lazygit.catppuccin.enable = true;
-    starship.catppuccin.enable = true;
-    yazi.catppuccin.enable = true;
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
   };
 }
