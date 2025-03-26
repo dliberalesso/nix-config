49699dec2489766cbd50bfe105bab5c765676620
Douglas Liberalesso <dliberalesso@gmail.com>
2025-03-25 22:31:18 -0300

    fix(hyperion): there is no way to load a config file via cli

--- nixos/nixavell/hyperion.nix
+++ nixos/nixavell/hyperion.nix
@@ -5,12 +5,11 @@
     hyperion-ng
   ];
 
-  #FIXME: json path should come from the store
   systemd.services.hyperion = {
     enable = true;
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.hyperion-ng}/bin/hyperiond --userdata /tmp/hyperion --service #/etc/nix/hyperion.json";
+      ExecStart = "${pkgs.hyperion-ng}/bin/hyperiond --service";
       ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
       Restart = "on-failure";
       User = "root";
