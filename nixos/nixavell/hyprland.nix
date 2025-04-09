9733ff78df408e952b7130cb765a19f1ccecbb08
Douglas Liberalesso <dliberalesso@gmail.com>
2025-04-09 13:50:55 -0300

    feat(hyprland): add `hyprpaper`

--- nixos/nixavell/hyprland.nix
+++ nixos/nixavell/hyprland.nix
@@ -30,7 +30,26 @@ in
     # WARNING How are these autostarting?
     environment.systemPackages = with pkgs; [
       dunst
+      hyprpaper
       networkmanagerapplet
     ];
+
+    # https://search.nixos.org/options?query=systemd.user.services
+    # https://wiki.archlinux.org/title/Systemd/User
+    # https://github.com/Vladimir-csp/uwsm/blob/master/example-units/hyprpaper.service:
+    systemd.user.services.hyprpaper = {
+      enable = true;
+      documentation = [ "man:hyprpaper(1)" ];
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      description = "Hyprpaper wallpaper utility for Hyprland";
+      serviceConfig = {
+        Type = "exec";
+        ExecCondition = ''${pkgs.systemd}/lib/systemd/systemd-xdg-autostart-condition "Hyprland" ""'';
+        ExecStart = "${pkgs.hyprpaper}/bin/hyprpaper";
+        Restart = "on-failure";
+        Slice = "background-graphical.slice";
+      };
+    };
   };
 }
