0ca51daf41159a1188da2182b61e85e9542d2e70
Douglas Liberalesso <dliberalesso@gmail.com>
2025-04-18 10:54:30 -0300

    revert(home/fcitx5): deisable (for now) `fcitx5`

--- home/nixavell.nix
+++ home/nixavell.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   ...
 }:
 {
@@ -10,16 +9,18 @@
     ./scripts
   ];
 
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-
-    fcitx5 = {
-      addons = with pkgs; [
-        fcitx5-gtk
-        kdePackages.fcitx5-qt
-      ];
-
-      waylandFrontend = true;
-    };
-  };
+  # TODO: add this to a specialization
+  # WARN: don't forget to start it in hyprland.conf
+  # i18n.inputMethod = {
+  #   enabled = "fcitx5";
+  #
+  #   fcitx5 = {
+  #     addons = with pkgs; [
+  #       fcitx5-gtk
+  #       kdePackages.fcitx5-qt
+  #     ];
+  #
+  #     waylandFrontend = true;
+  #   };
+  # };
 }
