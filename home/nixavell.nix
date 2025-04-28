05d7848f4b1bffcae8ca420b2dc0c4a80ffa28c0
Douglas Liberalesso <dliberalesso@gmail.com>
2025-04-28 13:50:46 -0300

    refactor(home/nixavell): remove commented out `fcitx5`

--- home/nixavell.nix
+++ home/nixavell.nix
@@ -1,26 +1,8 @@
 {
-  ...
-}:
-{
   imports = [
     ./common
     ./hyprde
     ./programs
     ./scripts
   ];
-
-  # TODO: add this to a specialization
-  # WARN: don't forget to start it in hyprland.conf
-  # i18n.inputMethod = {
-  #   enabled = "fcitx5";
-  #
-  #   fcitx5 = {
-  #     addons = with pkgs; [
-  #       fcitx5-gtk
-  #       kdePackages.fcitx5-qt
-  #     ];
-  #
-  #     waylandFrontend = true;
-  #   };
-  # };
 }
