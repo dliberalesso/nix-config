921b84b42d5daeace88ba37015573fd68b62237d
Douglas Liberalesso <dliberalesso@gmail.com>
2025-05-04 17:28:19 -0300

    fix(flake): fix the usage of `cache.nixos.org` by adding the corresponding key

--- flake.nix
+++ flake.nix
@@ -3,18 +3,21 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org"
+      "https://dliberalesso.cachix.org"
+      "https://catppuccin.cachix.org"
+      "https://hyprland.cachix.org"
       "https://cachix.cachix.org"
       "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://dliberalesso.cachix.org"
+      "https://cache.nixos.org/"
     ];
 
     extra-trusted-public-keys = [
+      "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
