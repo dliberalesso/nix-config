425301300e0448c7d39dcc1844480d086d86aa6f
Douglas Liberalesso <dliberalesso@gmail.com>
2025-05-31 23:39:47 -0300

    revert(core): remove `skim` and bring `fzf` back
    Refs: b516dc1

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -5,20 +5,19 @@
     ./bat.nix
     ./catppuccin.nix
     ./cli.nix
+    ./fzf.nix
     ./git.nix
     ./home.nix
     ./nh.nix
     ./nix.nix
     ./shell.nix
-    ./skim.nix
     ./tealdeer.nix
   ];
 
   boot.tmp.cleanOnBoot = true;
 
-  programs = {
-    skim.enable = true;
+  system = {
+    rebuild.enableNg = true;
+    switch.enableNg = true;
   };
-
-  system.rebuild.enableNg = true;
 }
