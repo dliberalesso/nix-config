53014800d973d10a85ac047ce5c7e181c44706a2
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-01 22:48:57 -0300

    feat(ci): build `packages` and `devShells` too

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -36,21 +36,15 @@ in
       lazy-trees = true;
 
       substituters = [
+        "https://dliberalesso.cachix.org"
         "https://nix-community.cachix.org"
         "https://cachix.cachix.org"
-        "https://dliberalesso.cachix.org"
-        "https://catppuccin.cachix.org"
-        "https://install.determinate.systems"
-        "https://hyprland.cachix.org"
       ];
 
       trusted-public-keys = [
+        "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
-        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
