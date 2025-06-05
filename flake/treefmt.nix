a9b8964bdc8674245457d629778f5113a14daa89
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-05 21:44:31 +0000

    refactor: use `lib.optionalAttrs` when possible

--- flake/treefmt.nix
+++ flake/treefmt.nix
@@ -3,60 +3,55 @@
   lib,
   ...
 }:
-let
-  hasTreefmt = inputs.treefmt-nix ? flakeModule;
-in
-{
-  imports = lib.optionals hasTreefmt [
+lib.optionalAttrs (inputs.treefmt-nix ? flakeModule) {
+  imports = [
     inputs.treefmt-nix.flakeModule
   ];
 
-  perSystem = lib.mkIf hasTreefmt (
+  perSystem =
     {
       config,
       ...
     }:
-    let
-      hasFlakeRoot = config ? flake-root;
-    in
     {
-      treefmt = {
-        projectRootFile = lib.mkIf hasFlakeRoot config.flake-root.projectRootFile;
+      treefmt =
+        lib.optionalAttrs (config ? flake-root) {
+          inherit (config.flake-root) projectRootFile;
+        }
+        // {
+          flakeCheck = !(config ? pre-commit);
 
-        flakeCheck = !(config ? pre-commit);
+          settings.global.excludes = [
+            "*.age" # Age encrypted files
+          ];
 
-        settings.global.excludes = [
-          "*.age" # Age encrypted files
-        ];
+          programs = {
+            actionlint.enable = true;
 
-        programs = {
-          actionlint.enable = true;
+            deadnix.enable = true;
+            nixfmt.enable = true;
+            statix.enable = true;
 
-          deadnix.enable = true;
-          nixfmt.enable = true;
-          statix.enable = true;
-
-          prettier = {
-            enable = true;
-            settings.editorconfig = true;
-          };
-
-          shellcheck.enable = true;
-          shfmt.enable = true;
-
-          stylua = {
-            enable = true;
-            settings = {
-              indent_type = "Spaces";
-              indent_width = 2;
-              call_parentheses = "Always";
-              collapse_simple_statement = "Always";
+            prettier = {
+              enable = true;
+              settings.editorconfig = true;
             };
-          };
 
-          taplo.enable = true;
+            shellcheck.enable = true;
+            shfmt.enable = true;
+
+            stylua = {
+              enable = true;
+              settings = {
+                indent_type = "Spaces";
+                indent_width = 2;
+                call_parentheses = "Always";
+                collapse_simple_statement = "Always";
+              };
+            };
+
+            taplo.enable = true;
+          };
         };
-      };
-    }
-  );
+    };
 }
