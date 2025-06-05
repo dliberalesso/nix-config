d8d2763d622db57c984a9a67c78f79a3bd55686f
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-05 10:57:54 -0300

    feat(flake): automatically toggle `debug`

--- flake/default.nix
+++ flake/default.nix
@@ -10,7 +10,8 @@
       ../packages
     ];
 
-  debug = true;
+  # WARN: Keep this to help debugging in the REPL
+  debug = false;
 
   # TODO: Should be a merge from the values set on hosts
   systems = [ "x86_64-linux" ];
