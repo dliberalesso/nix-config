c35c3c55b3a0eeb792fa96423127bbb11080e1cd
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-06 19:32:38 -0300

    refactor(flake): remove unnecessary `lib.optionalAttrs`

--- flake/pre-commit.nix
+++ flake/pre-commit.nix
@@ -24,30 +24,27 @@ lib.optionalAttrs (inputs.git-hooks ? flakeModule) {
           "^.patch"
         ];
 
-        hooks =
-          lib.optionalAttrs (config ? treefmt) {
-            treefmt.enable = true;
-            treefmt.package = config.treefmt.build.wrapper;
-          }
-          // {
-            commitizen.enable = true;
+        hooks = {
+          treefmt.enable = config ? treefmt;
 
-            editorconfig-checker.enable = true;
+          commitizen.enable = true;
 
-            # General use pre-commit hooks
-            trim-trailing-whitespace.enable = true;
-            mixed-line-endings.enable = true;
-            end-of-file-fixer.enable = true;
-            check-executables-have-shebangs.enable = true;
-            check-added-large-files.enable = true;
+          editorconfig-checker.enable = true;
 
-            gitleaks = {
-              enable = true;
-              name = "gitleaks";
-              entry = "${pkgs.gitleaks}/bin/gitleaks protect --verbose --redact --staged";
-              pass_filenames = false;
-            };
+          # General use pre-commit hooks
+          trim-trailing-whitespace.enable = true;
+          mixed-line-endings.enable = true;
+          end-of-file-fixer.enable = true;
+          check-executables-have-shebangs.enable = true;
+          check-added-large-files.enable = true;
+
+          gitleaks = {
+            enable = true;
+            name = "gitleaks";
+            entry = "${pkgs.gitleaks}/bin/gitleaks protect --verbose --redact --staged";
+            pass_filenames = false;
           };
+        };
       };
     };
 }
