f2c0cd87f13f810bb4796485bb7156dd78c1ca0b
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-08 15:02:19 -0300

    fix(core): 🐛 use `delta` as the `lazygit` pager only if `git.delta = true`

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -3,45 +3,57 @@
   user,
   ...
 }:
-hm {
-  programs = {
-    gh.enable = true;
+hm (
+  {
+    config,
+    lib,
+    pkgs,
+    ...
+  }:
+  {
+    home.packages = [ pkgs.koji ];
+    programs = {
+      gh.enable = true;
 
-    git = {
-      enable = true;
+      git = {
+        enable = true;
 
-      delta.enable = true;
+        delta.enable = true;
 
-      extraConfig = {
-        init.defaultBranch = "main";
-        push.autoSetupRemote = true;
-        url."https://github.com/".insteadOf = "git://github.com/";
-      };
-
-      ignores = [
-        ".direnv"
-        "result"
-      ];
-
-      lfs.enable = true;
-
-      userName = user.name;
-      userEmail = user.email;
-    };
-
-    lazygit = {
-      enable = true;
-
-      settings = {
-        git.paging = {
-          colorArg = "always";
-          pager = "delta --dark --paging=never";
+        extraConfig = {
+          init.defaultBranch = "main";
+          push.autoSetupRemote = true;
+          url."https://github.com/".insteadOf = "git://github.com/";
         };
 
-        os.editPreset = "nvim-remote";
+        ignores = [
+          ".direnv"
+          "result"
+        ];
 
-        update.method = "never";
+        lfs.enable = true;
+
+        userName = user.name;
+        userEmail = user.email;
+      };
+
+      lazygit = {
+        enable = true;
+
+        settings = {
+          git.paging =
+            {
+              colorArg = "always";
+            }
+            // lib.optionalAttrs config.programs.git.delta.enable {
+              pager = "${lib.getExe pkgs.delta} --dark --paging=never";
+            };
+
+          os.editPreset = "nvim-remote";
+
+          update.method = "never";
+        };
       };
     };
-  };
-}
+  }
+)
