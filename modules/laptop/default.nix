1d58cc44bc47fa36e3b81d2024cad6a79b52d4ca
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-08 13:26:27 -0300

    refactor(laptop): there is no need to use "${value}" when value is already a string

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -22,7 +22,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user.username} = {
     isNormalUser = true;
-    description = "${user.name}";
+    description = user.name;
 
     extraGroups = [
       "audio"
