5dcd188ec94e55d0f1f2bf69e49f8a1441f9de15
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-10 11:51:21 -0300

    feat: add custom command to chose a conventional commit type

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -40,6 +40,79 @@ hm (
         enable = true;
 
         settings = {
+          customCommands = [
+            {
+              key = "C";
+              description = "conventional commit";
+              context = "files";
+              prompts = [
+                {
+                  type = "menu";
+                  title = "Select the type of change you are committing.";
+                  key = "Type";
+                  options = [
+                    {
+                      name = "Feature";
+                      description = "a new feature";
+                      value = "feat";
+                    }
+                    {
+                      name = "Fix";
+                      description = "a bug fix";
+                      value = "fix";
+                    }
+                    {
+                      name = "Documentation";
+                      description = "Documentation only changes";
+                      value = "docs";
+                    }
+                    {
+                      name = "Styles";
+                      description = "Styling changes (white-space, formatting, etc)";
+                      value = "style";
+                    }
+                    {
+                      name = "Code Refactoring";
+                      description = "Refactoring code";
+                      value = "refactor";
+                    }
+                    {
+                      name = "Performance Improvements";
+                      description = "Performance improvements";
+                      value = "perf";
+                    }
+                    {
+                      name = "Tests";
+                      description = "Test additions or fixes";
+                      value = "test";
+                    }
+                    {
+                      name = "Builds";
+                      description = "Build system or dependency changes";
+                      value = "build";
+                    }
+                    {
+                      name = "Continuous Integration";
+                      description = "CI configuration changes";
+                      value = "ci";
+                    }
+                    {
+                      name = "Chores";
+                      description = "Other non-code changes";
+                      value = "chore";
+                    }
+                    {
+                      name = "Reverts";
+                      description = "Reverting changes";
+                      value = "revert";
+                    }
+                  ];
+                }
+              ];
+              command = "echo '{{ .Form.Type }}: ' > .git/LAZYGIT_PENDING_COMMIT";
+            }
+          ];
+
           git.paging =
             {
               colorArg = "always";
