e462f47d6663822915b0675d8cee9eaeffa8e866
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-13 15:15:08 -0300

    🚸 feat: Use `lazymoji` as a customCommand in `lazygit`

--- modules/core/git.nix
+++ modules/core/git.nix
@@ -43,83 +43,24 @@ hm (
           customCommands = [
             {
               key = "C";
-              description = "conventional commit";
+              description = "Use lazymoji";
               context = "files";
-              prompts = [
-                {
-                  type = "menu";
-                  title = "Select the type of change you are committing.";
-                  key = "Type";
-                  options = [
-                    {
-                      name = "Feature";
-                      description = "a new feature";
-                      value = "feat";
-                    }
-                    {
-                      name = "Fix";
-                      description = "a bug fix";
-                      value = "fix";
-                    }
-                    {
-                      name = "Documentation";
-                      description = "Documentation only changes";
-                      value = "docs";
-                    }
-                    {
-                      name = "Styles";
-                      description = "Styling changes (white-space, formatting, etc)";
-                      value = "style";
-                    }
-                    {
-                      name = "Code Refactoring";
-                      description = "Refactoring code";
-                      value = "refactor";
-                    }
-                    {
-                      name = "Performance Improvements";
-                      description = "Performance improvements";
-                      value = "perf";
-                    }
-                    {
-                      name = "Tests";
-                      description = "Test additions or fixes";
-                      value = "test";
-                    }
-                    {
-                      name = "Builds";
-                      description = "Build system or dependency changes";
-                      value = "build";
-                    }
-                    {
-                      name = "Continuous Integration";
-                      description = "CI configuration changes";
-                      value = "ci";
-                    }
-                    {
-                      name = "Chores";
-                      description = "Other non-code changes";
-                      value = "chore";
-                    }
-                    {
-                      name = "Reverts";
-                      description = "Reverting changes";
-                      value = "revert";
-                    }
-                  ];
-                }
-              ];
-              command = "echo '{{ .Form.Type }}: ' > .git/LAZYGIT_PENDING_COMMIT";
+              command = "${lib.getExe pkgs.lazymoji} -l";
+              output = "terminal";
             }
           ];
 
-          git.paging =
-            {
-              colorArg = "always";
-            }
-            // lib.optionalAttrs config.programs.git.delta.enable {
-              pager = "${lib.getExe pkgs.delta} --dark --paging=never";
-            };
+          git = {
+            paging =
+              {
+                colorArg = "always";
+              }
+              // lib.optionalAttrs config.programs.git.delta.enable {
+                pager = "${lib.getExe pkgs.delta} --dark --paging=never";
+              };
+
+            parseEmoji = true;
+          };
 
           os.editPreset = "nvim-remote";
 
