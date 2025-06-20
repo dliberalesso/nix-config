96c20f5908b6bf0162c1c7e7d51fd097199a4cff
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-20 18:50:44 +0000

    ✨ feat: Enable `redistributableFirmware`

--- modules/hardware/intel.nix
+++ modules/hardware/intel.nix
@@ -2,7 +2,12 @@
   unify.modules.laptop.nixos = {
     hardware = {
       enableAllFirmware = true;
+      enableRedistributableFirmware = true;
+
       cpu.intel.updateMicrocode = true;
     };
+
+    # enableAllFirmware depends on this
+    nixpkgs.config.allowUnfree = true;
   };
 }
