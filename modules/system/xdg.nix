c2906862413dccd1745ce26e0172cac4381c34bd
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-27 19:46:22 -0300

    🚸 feat: Set `xdg.userDirs`

--- modules/system/xdg.nix
+++ modules/system/xdg.nix
@@ -1,16 +1,46 @@
 {
-  unify.modules.hyprde.nixos =
-    {
-      pkgs,
-      ...
-    }:
-    {
-      xdg.portal = {
-        enable = true;
+  unify = {
+    home =
+      {
+        config,
+        ...
+      }:
+      let
+        mkDirStr = dir: "${config.home.homeDirectory}/${dir}";
+      in
+      {
+        xdg.userDirs = {
+          enable = true;
 
-        configPackages = [ pkgs.hyprland ];
+          documents = mkDirStr "documents";
+          desktop = mkDirStr "desktop";
+          download = mkDirStr "downloads";
+          music = mkDirStr "music";
+          pictures = mkDirStr "pictures";
+          publicShare = mkDirStr "public";
+          templates = mkDirStr "templates";
+          videos = mkDirStr "videos";
 
-        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+          extraConfig = {
+            XDG_PROJECTS_DIR = mkDirStr "projects";
+            XDG_MISC_DIR = mkDirStr "misc";
+          };
+        };
       };
-    };
+
+    modules.hyprde.nixos =
+      {
+        pkgs,
+        ...
+      }:
+      {
+        xdg.portal = {
+          enable = true;
+
+          configPackages = [ pkgs.hyprland ];
+
+          extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+        };
+      };
+  };
 }
