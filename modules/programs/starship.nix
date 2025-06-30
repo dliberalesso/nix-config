df6a87476661e2b0407dba20d4afbbaeaa5994d2
Douglas Liberalesso <dliberalesso@gmail.com>
2025-06-29 22:41:38 -0300

    ⚡️ perf: Remove preset generation from `starship` config

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -5,34 +5,11 @@
       lib,
       ...
     }:
-    let
-      package = pkgs.starship;
-      # package = pkgs.starship.overrideAttrs (oa: {
-      #   patches = (oa.patches or [ ]) ++ [
-      #     # TODO: maybe replace with npins
-      #     (pkgs.fetchpatch {
-      #       url = "https://github.com/starship/starship/pull/5772.patch";
-      #       hash = "sha256-DEazRBdybnPrqviBoKpAhHabulP8jE9rnN3ecKWs5Gg=";
-      #     })
-      #   ];
-      # });
-
-      nerdFontSymbols = lib.pipe package [
-        (p: "${lib.getExe p} preset nerd-font-symbols -o $out")
-        (c: pkgs.runCommand "gen-starship-nerdfonts-config" { } c)
-        builtins.readFile
-        builtins.fromTOML
-      ];
-    in
     {
       programs.starship = {
-        inherit package;
-
         enable = true;
 
-        settings = {
-          username.format = "[$user]($style) on ";
-        } // nerdFontSymbols;
+        settings = lib.importTOML "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml";
       };
     };
 }
