d83035a61e1428e1c229f26371e519abafdd5486
Douglas Liberalesso <dliberalesso@gmail.com>
2026-01-07 16:25:40 -0300

    ⬆️ build: Add temporary override for omnix to v1.3.2
    This override is necessary because v1.3.0 is failing to build due to juspay/omnix#494 and will stay in place until NixOS/nixpkgs#472294 (omnix: 1.3.0 -> 1.3.2) is merged.

--- modules/scripts/omnix-ci.nix
+++ modules/scripts/omnix-ci.nix
@@ -1,6 +1,7 @@
 {
   perSystem =
     {
+      lib,
       pkgs,
       ...
     }:
@@ -17,6 +18,33 @@
         text = ''
           om ci
         '';
+      };
+
+      # TODO: Remove this when https://github.com/NixOS/nixpkgs/pull/472294 is merged
+      overlayAttrs = {
+        omnix =
+          if lib.versionAtLeast pkgs.omnix.version "1.3.2" then
+            pkgs.omnix
+          else
+            pkgs.omnix.overrideAttrs (
+              finalAttrs: _prevAttrs: {
+                version = "1.3.2";
+
+                src = pkgs.fetchFromGitHub {
+                  owner = "juspay";
+                  repo = "omnix";
+                  tag = "v${finalAttrs.version}";
+                  hash = "sha256-D9rAVsSFooVWpSX//gTcRcmgiAjwZYNRMDIctMmwnho=";
+                };
+
+                cargoHash = "sha256-3zWbhuZzqkxgM0Js3luR6+Yr5/UGn1EoL6OqxPt94JM=";
+
+                cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                  inherit (finalAttrs) pname src version;
+                  hash = finalAttrs.cargoHash;
+                };
+              }
+            );
       };
     };
 }
