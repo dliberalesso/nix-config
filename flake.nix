4b4f548ca37cfb1aea9add61821edf766800d218
Douglas Liberalesso <dliberalesso@gmail.com>
2026-05-14 21:19:47 -0300

    🔧 chore: consolidate Nix cache settings in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixForge: My NixOS config";
+  description = "My NixOS config";
 
   outputs =
     inputs:
@@ -9,6 +9,26 @@
         ./modules
       ]
     );
+
+  nixConfig = {
+    always-allow-substitutes = true;
+
+    extra-substituters = [
+      "https://dliberalesso.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cachix.cachix.org"
+      "https://cache.numtide.com"
+      "https://catppuccin.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+    ];
+  };
 
   inputs = {
     nix-proxy-flake.url = "github:dliberalesso/nix-proxy-flake";
--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -11,8 +11,6 @@
 
         auto-optimise-store = true;
 
-        builders-use-substitutes = true;
-
         experimental-features = [
           "flakes"
           "nix-command"
@@ -22,12 +20,16 @@
           "https://dliberalesso.cachix.org"
           "https://nix-community.cachix.org"
           "https://cachix.cachix.org"
+          "https://cache.numtide.com"
+          "https://catppuccin.cachix.org"
         ];
 
         trusted-public-keys = [
           "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+          "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+          "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
         ];
 
         trusted-users = allowed-users;
