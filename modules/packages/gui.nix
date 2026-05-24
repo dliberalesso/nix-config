746f959dd0011eb43ba931310b6859d9eb666ba1
Douglas Liberalesso <dliberalesso@gmail.com>
2026-05-24 14:41:53 -0300

    ✨ feat: set google-chrome as default browser

--- modules/packages/gui.nix
+++ modules/packages/gui.nix
@@ -16,5 +16,11 @@
           vesktop # Discord
           ;
       };
+
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/http" = [ "google-chrome.desktop" ];
+        "x-scheme-handler/https" = [ "google-chrome.desktop" ];
+      };
     };
 }
